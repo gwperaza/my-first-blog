4837edcebd7196b802014e530c03c58aa6313dd9
gwperaza <gperaza@ivytech.edu>
2025-02-17 11:52:13 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3d%&92tok*ca#k(iu!f!*5*4+o1)!p@zx4kycs=x+3qdby-o0!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
